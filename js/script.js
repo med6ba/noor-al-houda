ca749cddff544f20106f915e5f30b25ab3d2abf3
medba <med6ba@gmail.com>
2025-02-24 13:34:54 +0000

    adding dark mode

--- js/script.js
+++ js/script.js
@@ -64,3 +64,23 @@ function zoomOut() {
     content.style.fontSize = newSize + "px";
   }
 }
+
+let darkmode = localStorage.getItem("dark-mode");
+const themeSwitch = document.getElementById("theme-switch");
+
+const enableDarkmode = () => {
+  document.body.classList.add("dark-mode");
+  localStorage.setItem("dark-mode", "active");
+};
+
+const disableDarkmode = () => {
+  document.body.classList.remove("dark-mode");
+  localStorage.setItem("dark-mode", null);
+};
+
+if (darkmode === "active") enableDarkmode();
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("dark-mode");
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
